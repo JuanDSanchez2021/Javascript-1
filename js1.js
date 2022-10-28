b2ccc87b4b6ce565e3a6668df51df8d417fe913b
JuanDSanchez2021 <92406360+JuanDSanchez2021@users.noreply.github.com>
2022-10-28 15:48:21 -0500

    Se actualizo js1

--- js1.js
+++ js1.js
@@ -1,45 +1,47 @@
+console.log("Hola Java Script");
+console.warn("Hola");
+console.error("Eroor");
+console.count("2");
 
- // @myinvite.digital
-
-// const { MutationObserverFactory } = require("@angular/cdk/observers");
-
-// Imprimir mensaje
-console.log("Hola Javascript");
-
-// Imprimir mensaje de alerta
-console.warn("mensaje");
-
-// Imprimir mensaje de error
-console.error("mensaje de error"); 
-
-// Saber tipo de dato typeof
-console.log("El tipo de dato del numero 30 es: " + typeof 30 + ".");
+console.log(typeof 4);
+console.log("Tipo de 34: " + typeof 34+".");
+/* se agrega ${} para poner la funcion dentro del texto */
 console.log(`Tipo de 34: ${typeof 34}.`);
+console.log();
+console.log('hola');
+console.log(`4 es igual a 3 ${4 == 3}`);
+console.log(`4 es mayor que 3 ${4 > 3}`);
+console.log(`4 es mayor igual a 3 ${4 >= 3}`);
+console.log(`4 es menor a 3 ${4 < 3}`);
+console.log(`4 es menor igual a 3${4 <= 3}`);
+console.log(`4 es distinto de 3 ${4 != 3}`);
+console.log(`4 es igual a 3 o 4 es mayor a 2 ${4 == 3 || 4 > 2}`);
+console.log(`4 es igual a 3 y 4 es mayor a 2 ${4 == 3 && 4 > 2}`);
+console.log(`4 es igual a 3 pero su resultado es el opuesto, y 4 es mayor a 2 ${!(4 == 3) && 4 > 2}`);
+console.log()
 
-// Operadores logicos
-// OR  ||
-// AND &&
-// NOT !
-console.log(`Ejemplo con operador logico: ${5 > 2 || 3 < 1}`);
+console.log(10 < 2 ? "OMG ES VERDAD NO WAY":"Es falso")
 
-// Operador ternario
-// Es como un if pero reducido, se usa asi: ? true: false
-console.log(10 < 2 ? "parte verdadera" : "parte falsa");
+let i;
+let nombre;
+let edad =33, ciudad="Monterrey";
 
-// if (10 < 2) {
-    
-// } else {
+/* CONSTANTES DEBES DE SER MAYOR */
+/* Las constantes no se les puede cambiar el valor */
 
-// }
+const FACTOR = 1.45;
+const VEL = 1500;
 
-// Declaracion de variables y constantes
-let temp = 10;
+console.log(FACTOR, VEL)
+
+console.log(edad, ciudad);
+
 let mensaje;
-
-mensaje = temp < 10 ? "Llevar sueter" : "No llevar sueter";
-
+let temp =4;
+mensaje = temp > 10? "Temperatura ambiente":"Temperatura fria";
 console.log(mensaje);
 
-// Declaracopm de constantes
-const SEMESTRE = 9;
-SEMESTRE = 3;
+const SEMESTRES =9;
+
+/* SI SE INTENTA MODIDIFICAR SE ROMPE EL PROGRAMA 
+SEMESTRES = 3; */
